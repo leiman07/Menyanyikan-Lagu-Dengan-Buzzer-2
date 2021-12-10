bb1434b3e25b749bfb3df51d44eadac58ebc17ef
Leiman GoVlog <31844101+leiman07@users.noreply.github.com>
2021-12-10 21:20:41 +0700

    Add files via upload

---
+++ music_buzzer2.ino
@@ -0,0 +1,90 @@
+//lagu Coffin
+
+//#include "pitches.h"
+
+//Pin sesuaikan pin kalian
+#include "pitches.h"
+
+int melody[] = {
+NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+NOTE_D5, NOTE_D5, NOTE_D5, NOTE_D5,
+NOTE_C5, NOTE_C5, NOTE_C5, NOTE_C5, 
+NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5, 
+NOTE_G5, NOTE_G5, NOTE_G5, NOTE_G5,
+NOTE_G5, NOTE_G5, NOTE_G5, NOTE_G5, 
+NOTE_G5, NOTE_G5, NOTE_G5, NOTE_G5, 
+NOTE_C5, NOTE_AS4, NOTE_A4, NOTE_F4,
+NOTE_G4, 0, NOTE_G4, NOTE_D5,
+NOTE_C5, 0, NOTE_AS4, 0,
+NOTE_A4, 0, NOTE_A4, NOTE_A4,
+NOTE_C5, 0, NOTE_AS4, NOTE_A4, 
+NOTE_G4,0, NOTE_G4, NOTE_AS5,
+NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5,
+NOTE_G4,0, NOTE_G4, NOTE_AS5,
+NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5,
+NOTE_G4, 0, NOTE_G4, NOTE_D5,
+NOTE_C5, 0, NOTE_AS4, 0,
+NOTE_A4, 0, NOTE_A4, NOTE_A4,
+NOTE_C5, 0, NOTE_AS4, NOTE_A4, 
+NOTE_G4,0, NOTE_G4, NOTE_AS5,
+NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5,
+NOTE_G4,0, NOTE_G4, NOTE_AS5,
+NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5
+
+};
+
+int noteDurations[] = {
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+4,4,4,4,
+};
+
+void setup() {
+
+}
+void loop() 
+{
+for (int thisNote = 0; thisNote < 112; thisNote++) 
+{
+
+int noteDuration = 750 / noteDurations[thisNote];
+tone(D1, melody[thisNote], noteDuration);
+
+int pauseBetweenNotes = noteDuration * 1.30;
+delay(pauseBetweenNotes);
+
+noTone(D1);}
+}
+
+//PROJECT : Program Menyanyikan Lagu Dengan Buzzer 2
+//DIBUAT  : TRIDIPI.PROJECT
+//TANGGAL : 07/12/2021
